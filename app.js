de0d257ba198f01ea2b6a383d2d52d4a39700e00
sabbir ahmmed <ahsabbir103@gmail.com>
2021-07-27 23:19:52 +0600

    setup updated

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 3000
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+  res.end();
 })
 
-app.listen(port, () => {
+
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+app.listen(port, host, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
