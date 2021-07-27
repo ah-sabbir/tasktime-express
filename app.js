0b4688957a225cc2eb82af4ef32bfa98bd4af13e
sabbir ahmmed <ahsabbir103@gmail.com>
2021-07-28 00:34:18 +0600

    mongodb added

--- app.js
+++ app.js
@@ -1,5 +1,40 @@
 const express = require('express')
 const app = express()
+const {MongoClient} = require('mongodb');
+
+
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+     const dbUrl = "mongodb+srv://ahsabbir103:sabbir.setpass.mongodb@cluster0.lypfc.mongodb.net/ahsabbir103?retryWrites=true&w=majority"
+ 
+
+    const client = new MongoClient(dbUrl);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+main().catch(console.error);
 
 
 app.get('/', (req, res) => {
