b734a2415233bf3c12461b35bf4b4cf96c002fcc
sabbir ahmmed <ahsabbir103@gmail.com>
2021-08-12 22:21:26 +0600

    gitignore added

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,72 +3,38 @@ const bcrypt    = require('bcryptjs')
 const jwt       = require('jsonwebtoken')
 
 
-
-const register = async (req,res,next)=>{
+const register = (req,res,next)=>{
     let body = {
        "name": req.body.name,
         "email": req.body.email,
         "password":req.body.password
     }
-
-    if (!(body.email && body.password)) {
-        return res.status(400).send({ error: "Data not formatted properly" });
-    }
-
-    //   const user = new User(body);
-    User.findOne({email:body.email})
-    .then(user=> {
-        if(user){
+    bcrypt.hash(body.password, 10, (err, hashedPass)=>{
+        if(err){
             res.json({
-                error:"user already exist"
+                error:err
             })
         }
+        let user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password:hashedPass
+        })
+        user.save()
+            .then(result=>{
+                res.status(201).json({
+                    message:"user created",
+                    user:result
+                })
+            })
+            .catch(error=>{
+                res.json({
+                    error:error,
+                    message: req.body.email
+                })
+            })
     })
-
-    //   .then(u=> res.json({error:"user already exist"}))
-      
-    const salt = await bcrypt.genSalt(10);
-    const user = new User(body)
-    user.password =  await bcrypt.hash(user.password, salt);
-    await user.save().then((doc) => res.status(201).send(doc));
-    
 }
-
-
-
-// const register = (req,res,next)=>{
-//     if(req.body.name && req.body.phone && req.body.email){
-//         console.log(req.body)
-//         res.json({
-//             "body":req.body
-//         })
-//     }
-    // bcrypt.hash(req.body.password, 10, (err, hashedPass)=>{
-    //     if(err){
-    //         res.json({
-    //             error:err
-    //         })
-    //     }
-    //     let user = new User({
-    //         name: req.body.name,
-    //         phone:req.body.phone,
-    //         email: req.body.email,
-    //         password: hashedPass
-    //     })
-        
-    //     user.save()
-    //     .then(user=>{
-    //         res.json({
-    //             message:'Successfully registered !'
-    //         })
-    //     })
-    //     .catch(error=>{
-    //         res.json({
-    //             message: "error "
-    //         })
-    //     })
-    // })
-// }
 
 
 const login = (req,res,next)=>{
